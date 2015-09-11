e04e55850ec620ed44cc23131157352aa354fa3e
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-11 14:01:45 -0700

    JQuery and CSS added.

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,18 @@
 var findReplace = function(string, find, replace) {
   var findall = new RegExp(find, "g")
   return string.replace(findall, replace);
-
 };
+
+$(document).ready(function() {
+  $("form#find-replace").submit(function(event) {
+    event.preventDefault()
+
+    var string = $("input#string").val();
+    var find = $("input#find").val();
+    var replace = $("input#replace").val();
+    var result = findReplace(string, find, replace);
+
+    $('#result').text(result);
+    event.preventDefault();
+  });
+});
--- spec/specs.js
+++ spec/specs.js
@@ -4,7 +4,7 @@ describe('findReplace', function() {
   });
 
   it("will find and replace all instances of a word in a string with another user specified word", function() {
-    expect(findReplace("Brandon is cool because his name is Brandon","Brandon", "Jimmy")).to.equal("Jimmy is cool because his name is Jimmy");
+    expect(findReplace("Brandon is cool because his name is Brandon","Brandon","Jimmy")).to.equal("Jimmy is cool because his name is Jimmy");
   });
 
 });
