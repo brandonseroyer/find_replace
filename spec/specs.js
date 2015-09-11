5fe6f18c21e0b038209b81678110ba625ed8f640
Brandon Seroyer <brandonseroyer@gmail.com>
2015-09-11 13:44:52 -0700

    2nd spec added.

--- spec/specs.js
+++ spec/specs.js
@@ -1,5 +1,10 @@
 describe('findReplace', function() {
-  it("will find and replace a word in a string", function() {
+  it("will find and replace a word in a string with another user specified word", function() {
     expect(findReplace("Brandon is cool","Brandon", "Nicole")).to.equal("Nicole is cool");
   });
+
+  it("will find and replace all instances of a word in a string with another user specified word", function() {
+    expect(findReplace("Brandon is cool because his name is Brandon","Brandon", "Nicole")).to.equal("Nicole is cool because her name is Nicole");
+  });
+
 });
